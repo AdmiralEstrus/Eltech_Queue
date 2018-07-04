645b50e4e9c4ea890da32dd5b6ed68bafcc672d7
Kiran <kiran@MacBook-Air-Kiran.local>
2018-07-04 05:14:51 +0300

    removed MVC, bugged

--- index.php
+++ index.php
@@ -1,7 +1,30 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(1);
-require_once __DIR__ . "/components/Router.php";
-require_once __DIR__ . "/components/DbConnection.php";
-$router = new Router();
-$router->run();
+$db = mysqli_connect('mysql-114993.srv.hoster.ru', 'srv114993_Kiran', 'Dbhtylh', 'srv114993_database');
+$result = mysqli_query($db,"SELECT * FROM queue WHERE id = 1");
+$fetch = $result->fetch_assoc();
+$currentNumber = $fetch['currentNumber'];
+
+if (isset($_POST['next'])) {
+    $currentNumber++;
+    mysqli_query($db, "UPDATE queue SET currentNumber = $currentNumber");
+}
+
+if (isset($_POST['reset'])) {
+    $currentNumber = 0;
+    mysqli_query($db, "UPDATE queue SET currentNumber = $currentNumber");
+}
+?>
+
+<center>
+    <h1>Следующий номер: <?= $currentNumber ?></h1>
+</center>
+
+<form action="" method="post">
+    <button type="submit" name="next">Следующий</button>
+</form>
+
+
+<form action="" method="post">
+    <button type="submit" name="reset">Сбросить</button>
+</form>
+z
